6e04606cde00640f33f6bbd17bb551f84f2c0aec
Ashley <Ashley>
2016-04-12 22:18:53 +1200

    adding validation to sign up invitation

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,8 +1,20 @@
+// app/controllers/index.js
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
-//Here we are controlling the disabled handlebar property in our index.hbs page
-isDisabled: true
+  emailAddress: '',
+
+  isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
+  isDisabled: Ember.computed.not('isValid'),
+
+  actions: {
+
+    saveInvitation() {
+      alert(`Saving of the following email address is in progress: ${this.get('emailAddress')}`);
+      this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
+      this.set('emailAddress', '');
+    }
+  }
 
 });
